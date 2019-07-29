495d6f1354f03c293478a68509678a1e9945b032
ueda-takahiro <ueda196@gmail.com>
2019-07-29 19:11:54 +0900

    デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,14 +56,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
@@ -72,8 +64,6 @@ set :default_env, {
 }
 
 set :linked_files, %w{ config/secrets.yml }
-
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
